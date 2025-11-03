a40a774c8531edece255ca7da3ee8ef08654739f
Bobby Kumar <bobbyyadav1950@gmail.com>
2025-11-03 11:43:55 +0530

    Uses Bow with 800 features and test size 0.3

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,8 +4,8 @@ import pandas as pd
 import pickle
 import os
 
-vectorized_train_data = pd.read_csv('data/interim/tfidf_train_data.csv')
-vectorized_test_data = pd.read_csv('data/interim/tfidf_test_data.csv')
+vectorized_train_data = pd.read_csv('data/interim/bow_train_data.csv')
+vectorized_test_data = pd.read_csv('data/interim/bow_test_data.csv')
 
 x_train = vectorized_train_data.drop(['labels'],axis=1)
 x_test = vectorized_test_data.drop(['labels'],axis=1)
